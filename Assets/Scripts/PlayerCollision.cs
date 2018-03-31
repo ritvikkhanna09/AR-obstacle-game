f4ac1cc627053e1d4f34d36f32d451e97535cb14
Ritvik Khanna <ritvikkhanna09@gmail.com>
2018-03-31 13:05:58 +0530

    initial commit

---
+++ Assets/Scripts/PlayerCollision.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerCollision : MonoBehaviour {
+
+	public PlayerMovement movement;
+
+	void OnCollisionEnter(Collision collisionInfo){
+		Debug.Log (collisionInfo.collider.name);
+		if (collisionInfo.collider.tag == "Obstacle"){
+			movement.enabled= false;
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+	public Rigidbody Rb;
+	public float ForwardForce = 2000f;
+	public float SideWaysForce = 500f;
+	
+	// Use this for initialization
+	void Start () { 
+
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		Rb.AddForce (0, 0, ForwardForce * Time.deltaTime);
+		float temp=Input.acceleration.x;	
+//		Debug.Log (temp);
+//		translate by x axis
+		transform.Translate(temp,0,0);
+
+		//rotating by y axis
+//		transform.Rotate(0,temp*10,0);
+
+//		if(Input.GetKey("d")){
+//			Rb.AddForce(SideWaysForce*Time.deltaTime,0,0,ForceMode.VelocityChange);
+//		}
+//		if(Input.GetKey("a")){
+//			Rb.AddForce(-SideWaysForce*Time.deltaTime,0,0,ForceMode.VelocityChange);
+//		}
+	}
+}
